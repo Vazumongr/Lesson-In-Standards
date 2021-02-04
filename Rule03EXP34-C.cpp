efa2948c2be8d06053d14fa177f776767d72e30a
Vazumongr <31901845+Vazumongr@users.noreply.github.com>
2021-02-04 17:23:09 -0500

    Added first violation
    Description is inside the block comment at the top of the file.

---
+++ Rule03EXP34-C.cpp
@@ -0,0 +1,61 @@
+// Troy Records Jr. 2020
+
+/*
+ * Line 47 down I am accessing (derefencing) a pointer without
+ * verifying whether it is a null or not. 
+ * C++ Standards
+ * Rule 03. EXP34-C. Do not dereference null pointers
+ */
+
+
+#include "DamageTextComponent.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Capstone/Widgets/DamageTextWidget.h"
+
+// Sets default values for this component's properties
+UDamageTextComponent::UDamageTextComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDamageTextComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UDamageTextComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UDamageTextComponent::SpawnDamageText(float DamageAmount)
+{
+	UDamageTextWidget* DamageText = CreateWidget<UDamageTextWidget>(GetWorld(), DamageTextClass);
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if(PlayerController == nullptr) return;
+	
+	DamageText->SetOwner(this, PlayerController);
+	DamageText->SetText(DamageAmount);
+	DamageText->AddToViewport();
+	
+	const FVector Location = GetComponentLocation();
+	FVector2D ScreenLocation;
+	PlayerController->ProjectWorldLocationToScreen(Location, ScreenLocation);
+
+	DamageText->SetPositionInViewport(ScreenLocation);
+	DamageText->Destruct();
+}
